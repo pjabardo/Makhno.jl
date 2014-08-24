2092376b716173e43099c9b1aaad5f1a88fe9789
Paulo Jabardo <pjabardo@yahoo.com.br>
2014-08-24 09:36:30 -0300

    New LAPACK functions.
    Added new LAPACK functions dealing with
    Choleksy trasnforms of packed symmetric
    matrices and banded matrices. Started
    working on matrix solving.

--- src/matrix.jl
+++ src/matrix.jl
@@ -15,7 +15,41 @@ type CholeskySolver <: DirectStaticCondensation
     
     nbe::Int
     nie::Int
-
-    A::Array{Float64,2}
+    
+    Abb::Array{Float64,2}
+    Aii::Array{Float64,2}
 end
 
+function sym2pckd!(A, AP, uplo='L')
+    n = size(A,1)
+    np = div(n*(n+1),2)
+        
+    AP = zeros(eltype(A), np)
+
+    if uplo=='U' 
+        count = 1
+        for i = 1:n
+            for j = 1:i
+                AP[count] = A[j,i]
+                count += 1
+            end
+        end
+    else
+        count = 1
+        for i = 1:n
+            for j = i:n
+                AP[count] = A[i,j]
+                count += 1
+            end
+        end
+    end
+
+    return AP
+end
+
+
+
+    
+#function add_matrix(solver::CholeskySolver, Abb, Aib, Aii, is_dirichilet)
+
+    
